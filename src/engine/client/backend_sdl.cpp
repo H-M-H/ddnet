94c99d6c99b2fc48769c9e44dd6861973a6d2cef
H-M-H <henry@freedesk.net>
2016-08-27 21:10:27 +0200

    raw H.264 --> mp4 \o/

--- src/engine/client/backend_sdl.cpp
+++ src/engine/client/backend_sdl.cpp
@@ -37,7 +37,10 @@
 #include <engine/shared/config.h>
 #include <base/tl/threading.h>
 
-#include "video.h"
+#if defined(CONF_VIDEORECORDER)
+	#include "video.h"
+#endif
+
 #include "graphics_threaded.h"
 #include "backend_sdl.h"
 
@@ -1458,8 +1461,10 @@ CCommandProcessorFragment_SDL::CCommandProcessorFragment_SDL()
 
 bool CCommandProcessorFragment_SDL::RunCommand(const CCommandBuffer::SCommand *pBaseCommand)
 {
-	if (CVideo::Current())
-		CVideo::Current()->nextFrame();
+	#if defined(CONF_VIDEORECORDER)
+		if (CVideo::Current())
+			CVideo::Current()->nextFrame();
+	#endif
 
 	switch(pBaseCommand->m_Cmd)
 	{
